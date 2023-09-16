ba7e62f2ccf9a3538545de5a4537f547102e4b0b
Tan Yu Ren <tanyuren1994@gmail.com>
2023-09-16 15:07:00 +0800

    Update

--- src/main/java/com/example/demo/DemoApp1.java
+++ src/main/java/com/example/demo/DemoApp1.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApp1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApp1.class, args);
+//		SpringApplication.run(DemoApp1.class, args);
+
+		try {
+			int result = 10 / 0;
+		} catch (ArithmeticException e) {
+			System.out.println("You can't divide by Zero");
+		}
 
 	}
 
