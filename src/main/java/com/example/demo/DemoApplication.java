cdeebaa2fc4c747e9feb7c244424632ee87e8b38
tanyuren1994 <tanyuren1994@gmail.com>
2023-09-16 11:28:38 +0800

    demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,31 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+		int arrayLength = scanner.nextInt();
+		int[] arr = new int[arrayLength];
+		for (int i=0; i<arrayLength; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		int maxNumber = arr[0];
+		int index = 0;
+		for (int i=1; i<arrayLength; i++) {
+			if (arr[i] > maxNumber){
+				index = i;
+				maxNumber = arr[i];
+			}
+		}
+		System.out.println(index);
+
+
 	}
 
 }
