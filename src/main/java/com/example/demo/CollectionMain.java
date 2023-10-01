fb9a64bacdcaf8c10419e7aabe46a1141546143c
tanyuren1994 <tanyuren1994@gmail.com>
2023-10-01 21:46:13 +0800

    demo

--- src/main/java/com/example/demo/CollectionMain.java
+++ src/main/java/com/example/demo/CollectionMain.java
@@ -6,29 +6,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Scanner;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
 @SpringBootApplication
-public class CollectionMain {
-
-	public static void main(String[] args) {
-//		SpringApplication.run(DemoApp1.class, args);
-		Scanner scanner = new Scanner(System.in);
-
-		Collection<String> phones = Arrays.asList(scanner.nextLine().split("\\s+"));
-		Collection<String> blockList = Arrays.asList(scanner.nextLine().split("\\s+"));
-
-		Collection<String> nonBlockedPhones = CollectionUtils.filterPhones(
-				new ArrayList<>(phones), new ArrayList<>(blockList));
-
-		System.out.println(nonBlockedPhones.stream()
-				.map(Object::toString)
-				.collect(Collectors.joining(" ")));
-
-	}
-}
+//public class CollectionMain {
+//
+//	public static void main(String[] args) {
+////		SpringApplication.run(DemoApp1.class, args);
+//		Scanner scanner = new Scanner(System.in);
+//
+//		Collection<String> phones = Arrays.asList(scanner.nextLine().split("\\s+"));
+//		Collection<String> blockList = Arrays.asList(scanner.nextLine().split("\\s+"));
+//
+//		Collection<String> nonBlockedPhones = CollectionUtils.filterPhones(
+//				new ArrayList<>(phones), new ArrayList<>(blockList));
+//
+//		System.out.println(nonBlockedPhones.stream()
+//				.map(Object::toString)
+//				.collect(Collectors.joining(" ")));
+//
+//	}
+//}
 
 class CollectionUtils {
 
@@ -39,5 +39,25 @@ class CollectionUtils {
 		phones = phones.stream().filter(blacklist::contains).collect(Collectors.toList());
 
 		return phones;
+	}
+}
+
+public class CollectionMain {
+	public static void main(String[] args) {
+		ArrayList<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+		for (String name : nameList) {
+			System.out.println(name);
+		}
+
+		List<Integer> list = new ArrayList<>();
+		list.add(2);
+		list.add(0);
+		list.add(1);
+		list.add(7);
+		System.out.println(list);
+
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+		numbers.add(6);
+		System.out.println(numbers);
 	}
 }
